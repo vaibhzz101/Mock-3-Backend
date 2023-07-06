c7b6b2de0286ea3413d8be66fd92d8eb2a7dcc17
Vaibhav Shevne <112811407+vaibhzz101@users.noreply.github.com>
2023-07-06 15:00:48 +0530

    Add files via upload

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -19,17 +19,27 @@ BookRouter.post("/addbook", async (req, res) => {
 
         const ifBookPresent = await BookModel.findOne({ title, author });
         if (ifBookPresent) {
-            return res.status(400).json({ msg: "Book already present" });
+            return res.status(400).json({ msg: "Book present add new book !!" });
         } else {
             const addBook = new BookModel({ title, author, genre, description, price });
             await addBook.save();
 
-            res.send({ msg: "Book added successfully" });
+            res.send({ msg: "Book added  !! " });
         }
     } catch (err) {
         res.status(500).send({ msg: "Could not add book", error: err.message });
     }
 });
+BookRouter.delete("/delete-book/:id", async (req, res) => {
+
+    const ID = req.params.id;
+    try {
+      await BookModel.findByIdAndDelete({ _id: ID });
+      res.send({ msg: "Book has been deleted " });
+    } catch (err) {
+      res.send({ msg: "Cannot delete the book", error: err.message });
+    }
+  });
 
 module.exports = {
     BookRouter
